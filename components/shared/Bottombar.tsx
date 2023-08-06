b19016e234cb52d2039dd5e1c5c693f761fa447d
konrad.kaluzny <konrad.kaluzny@hotmail.com>
2023-08-06 13:48:39 +0200

    structure

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
 
-type Props = {}
+import { sidebarLinks } from "@/data";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter, usePathname } from "next/navigation";
+import { SignedIn, SignOutButton } from "@clerk/nextjs";
+
+type Props = {};
 
 export default function Bottombar({}: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+
   return (
-    <div>Bottombar</div>
-  )
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-light-1 text-subtle-medium max-sm:hidden">
+                {link.label.split(/\s+/)[0]}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
